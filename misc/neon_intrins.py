53194d046f63086be41c7265dbeb3885eaa3a82d
yrp <yrp604@protonmail.com>
2021-02-03 21:38:33 -0800

    Squashed 'arch-arm64/' changes from 6bef150..a227bfc
    a227bfc Merge remote-tracking branch 'yrp604/revert-operand-name' into dev
    e4d5cc1 Lift more exotic fmov encodings, issue #24
    e2025d7 Revert the operand name change to restore pointer-free-ness
    14d7981 Generate MTE lifting tests
    8aeca0a Add memory tagging extension (MTE) notes
    8b2fb1f Merge remote-tracking branch 'yrp604/dis-refactor2' into dev
    f81e992 Merge remote-tracking branch 'yrp604/msvc-fixes-i-should-have-documented-better' into dev
    a447980 Add generated intrinsics (WIP)
    0672a94 Make registers fall in line
    975ea60 Remove final string ops from disassembler
    e68bf9e Remove stdio.h
    7eba6e2 Fix MSVC build
    63a22fb Lift & test sha* instructions, fix #23
    ee0d84c Generated NeonGetIntrinsicInputs(), NeonGetIntrinsicOutputs(), etc.
    69ab3ef Attempt output NEON intrinsic generation
    08e407d Generate NEON input lists
    e531410 Improved output type parsing in NEON intrins generation
    a6f5ba3 NEON intrinsic read/write types by function signature
    
    git-subtree-dir: arch-arm64
    git-subtree-split: a227bfc103c14d5ad507acdaedb6ca3a2a10f6c3

--- misc/neon_intrins.py
+++ misc/neon_intrins.py
@@ -4,6 +4,8 @@
 import re
 import sys
 
+from collections import OrderedDict
+
 # SMMLA Vd.4S,Vn.16B,Vm.16B -> Vd.4S
 def get_destination_reg(asig):
 	try:
@@ -60,71 +62,200 @@ def get_write_size(asig):
 
 	return get_reg_size(reg0)
 
-with open('neon_intrins.c') as fp:
-	lines = [l.strip() for l in fp.readlines()]
+def type_to_binja_types(ntype):
+	# remove pointer
+	if ntype.endswith(' const *'):
+		ntype = ntype[0:-8]
+	if ntype.endswith(' *'):
+		ntype = ntype[0:-2]
 
-seen = set()
-intrin_defines = [] # parallel arrays to maintain order from all_neon_intrinsics.c
-intrin_names = []
-intrin_asigs = []
+	binja_type = 'Float' if 'float' in ntype else 'Int'
 
-for l in lines:
-	if 'reinterpret' in l: continue
-	(fsig, asig) = l.split('; // ')
+	# int (for lane or immediate)
+	if ntype == 'int':
+		return ['Type::IntegerType(4)']
 
-	m = re.match(r'^\w+ (\w+)\(.', fsig)
-	fname = m.group(1)
+	# multiple packed, eg: "uint8x8x2_t"
+	m = re.match(r'^(\w+?)(\d+)x(\d+)x(\d+)_t$', ntype)
+	if m:
+		(base, bit_width, npacked, nregs) = m.group(1,2,3, 4)
+		return ['Type::%sType(%d)' % (binja_type, int(bit_width)*int(npacked)/8)]*int(nregs)
 
-	if fname in seen:
-		continue
-	else:
-		seen.add(fname)
+	# packed in registers, eg: "int8x8_t"
+	m = re.match(r'^(\w+?)(\d+)x(\d+)_t$', ntype)
+	if m:
+		(base, bit_width, npacked) = m.group(1,2,3)
+		return ['Type::%sType(%d)' % (binja_type, int(bit_width)*int(npacked)/8)]
 
-	#print('-%s- -%s- -%s-' % (fsig, fname, asig))
-	intrin_defines.append('ARM64_INTRIN_%s' % fname.upper())
-	intrin_names.append(fname)
-	if asig.startswith('RESULT['): asig = None # array-like looping not yet supported
-	intrin_asigs.append(asig)
+	# simple, eg: "int8_t"
+	m = re.match(r'^(\w+?)(\d+)_t$', ntype)
+	if m:
+		(base, bit_width) = m.group(1,2)
+		return ['Type::%sType(%d)' % (binja_type, int(bit_width)/8)]
 
-if sys.argv[1] in ['enum', 'enumeration']:
-	# for enum NeonIntrinsic : uint32_t ...
-	for x in intrin_defines:
-		extra = '=ARM64_INTRIN_NORMAL_END' if x==intrin_defines[0] else ''
-		print('\t%s%s,' % (x, extra))
+	print('cannot convert neon type %s into binja type' % ntype)
+	sys.exit(-1)
 
-elif sys.argv[1] in ['name', 'names']:
-	# for GetIntrinsicName(uint32_t intrinsic)
-	for i in range(len(intrin_defines)):
-		print('\t\tcase %s: return "%s";' % (intrin_defines[i], intrin_names[i]))
+# given an intrinsic's name, argument types, and return type, compute
+# the binja intrinsic input types
+def resolve_input_types(name, arg_types, return_type):
+	result = []
 
-elif sys.argv[1] in ['all', 'define', 'defines']:
-	# for GetAllIntrinsics()
-	i = 0
-	while i<len(intrin_defines):
-		print('\t\t' + ', '.join(intrin_defines[i:i+3]) + ',')
-		i += 3
+	for at in arg_types:
+		if at.endswith(' *'):
+			# eg: int32x4x2_t vld2q_s32(int32_t const * ptr);
+			assert ('ld' in name) or ('st' in name)
+			result.extend(neon_type_to_binja_types(return_type))
+		else:
+			result.extend(neon_type_to_binja_types(at))
 
-elif sys.argv[1] in ['input', 'inputs']:
-	pass
+	return result
 
-elif sys.argv[1] in ['output', 'outputs']:
-	size_to_cases = {}
+if __name__ == '__main__':
+	# parse neon_intrins.c into a "database"
+	with open('neon_intrins.c') as fp:
+		lines = [l.strip() for l in fp.readlines()]
 
-	# for GetIntrinsicOutputs()
-	for i in range(len(intrin_defines)):
-		asig = intrin_asigs[i]
-		if not asig: continue
-		reg_dest = get_destination_reg(asig)
-		wsize = get_write_size(asig)
+	db = OrderedDict()
 
-		if not wsize in size_to_cases:
-			size_to_cases[wsize] = []
+	for l in lines:
+		if 'reinterpret' in l: continue
+		if 'RESULT[' in l: continue
+		(fsig, asig) = l.split('; // ')
 
-		#size_to_cases[wsize].append('case %s: // writes %s (%d bytes)' % (intrin_defines[i], reg_dest, wsize))
-		size_to_cases[wsize].append('case %s:' % intrin_defines[i])
+		# function name
+		m = re.match(r'^(\w+) (\w+)\((.*)\)$', fsig)
+		fname = m.group(2)
+		if fname in db: continue
+		if asig.startswith('RESULT['): continue
 
-	for size in sorted(size_to_cases):
-		for case in size_to_cases[size]:
-			print('\t\t%s' % case)
+		# function arguments
+		fargs = [m.group(1)] + m.group(3).split(', ')
+		fargs = [x.replace('const ', '') for x in fargs]
 
-		print('\t\t\treturn {Type::IntegerType(%d, false)};' % size)
+		(operation, operands) = re.match(r'^(\w+?) (.*)$', asig).group(1, 2)
+		operands = operands.split(',')
+
+		db[fname] = OrderedDict({
+			          'fsig': fsig,
+		              'asig': asig,
+		              'define': 'ARM64_INTRIN_%s' % fname.upper(),
+		              'operation': 'ARM64_' + operation,
+		              'fargs': fargs,
+		              'operands': operands,
+		           })
+
+	cmd = sys.argv[1]
+
+	if cmd in ['dump']:
+		import pprint
+		pp = pprint.PrettyPrinter()
+		pp.pprint(db)
+
+	elif cmd in ['enum', 'enumeration']:
+		# for enum NeonIntrinsic : uint32_t ...
+		first = True
+		for fname in db:
+			extra = '=ARM64_INTRIN_NORMAL_END' if first else ''
+			print('\t%s%s,' % (db[fname]['define'], extra))
+			first = False
+
+	elif cmd in ['name', 'names']:
+		# for GetIntrinsicName(uint32_t intrinsic)
+		for fname in db:
+			print('\t\tcase %s: return "%s";' % (db[fname]['define'], fname))
+
+	elif cmd in ['all', 'define', 'defines']:
+		# for GetAllIntrinsics()
+		collection = [db[fname]['define'] for fname in db]
+		i = 0
+		while i<len(collection):
+			print('\t\t' + ', '.join(collection[i:i+3]) + ',')
+			i += 3
+
+	elif cmd in ['input', 'inputs']:
+		# for GetIntrinsicInputs()
+
+		# collect all unique write types
+		rtstrs = set(str(db[x]['binja_input_types']) for x in db)
+
+		# for each write type
+		for rtstr in sorted(rtstrs):
+			fnames = [x for x in db if str(db[x]['binja_input_types']) == rtstr]
+
+			# print cases in the db that have the same type
+			for fname in fnames:
+				print('\t\tcase %s:' % (db[fname]['define']))
+
+			print('\t\t\treturn {%s};' % (', '.join(db[fnames[0]]['binja_input_types'])))
+
+	elif cmd in ['output', 'outputs']:
+		# for GetIntrinsicOutputs()
+
+		# collect all unique write types
+		wtstrs = set(str(db[x]['binja_output_types']) for x in db)
+
+		# for each write type
+		for wtstr in sorted(wtstrs):
+			fnames = [x for x in db if str(db[x]['binja_output_types']) == wtstr]
+
+			# print cases in the db that have the same type
+			for fname in fnames:
+				print('\t\tcase %s:' % (db[fname]['define']))
+
+			print('\t\t\treturn {%s};' % (', '.join(db[fnames[0]]['binja_output_types'])))
+
+	elif cmd in ['implementation', 'code']:
+		# expects:
+		# std::vector<RegisterOrFlag> outputs
+		# std::vector<ExprId> inputs
+		for fname in db:
+			entry = db[fname]
+
+			print('\t\tcase %s:' % entry['operation'])
+			print('\t\t{')
+			print('\t\t\t// fsig: %s' % entry['fsig'])
+			print('\t\t\t// asig: %s' % entry['asig'])
+			print('\t\t\t// operands_n: %d' % entry['operands_n'])
+			print('\t\t\tadd_output(outputs, oper0, inst, INTRIN_TYPE_HINT_%s);' % (' '.join(entry['binja_output_types']).upper()))
+			for i in range(0, len(entry['binja_input_types'])):
+				print('\t\t\tadd_input(inputs, oper%d, inst, INTRIN_TYPE_HINT_%s);' % (i+1, entry['binja_input_types'][i].upper()))
+			print('\t\t\til.AddInstruction(il.Intrinsic(outputs, %s, inputs));' % entry['define'])
+			print('\t\t}')
+			print('\t\tbreak;')
+
+	elif cmd in ['test']:
+		for fname in db:
+			entry = db[fname]
+			fargs = entry['fargs']
+			operands = entry['operands']
+
+			print(entry['operation'])
+			print('fsig: %s' % entry['fsig'])
+			print('asig: %s' % entry['asig'])
+			print('fargs: %s' % fargs)
+			print('operands: %s' % operands)
+
+			# convert OPERATION X,Y,Z[lane] ->
+			#         OPERATION X,Y,Z,Z[lane]
+#			tmp = []
+#			for o in operands:
+#				m = re.match(r'^(.*)\[lane\d*\]$', o)
+#				if m:
+#					tmp.append(m.group(1))
+#					tmp.append('lane(%s)' % m.group(1))
+#				else:
+#					tmp.append(o)
+#			operands = tmp
+			# convert OPERATION X,Y,#0 ->
+			#         OPERATION X,Y
+			if re.match(r'^#\d+$', operands[-1]):
+				operands = operands[:-1]
+			#
+			if len(fargs) == len(operands)+1:
+				operands = [operands[0]] + operands
+
+			if len(operands) != len(fargs):
+				print('cant reconcile fargs and operands')
+				if not 'vcopy' in entry['fsig']:
+					sys.exit(-1)
